8156420c22c63c41fb3f6cb2c4c0bc10adf38f35
kingkonggrunt <kingkonggrunt@users.noreply.github.com>
2022-01-09 21:56:11 +1100

    feat: added md and py for starter notebook

---
+++ project-03-starter.py
@@ -0,0 +1,155 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light,md
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.5
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# <img src="http://imgur.com/1ZcRyrc.png" style="float: left; margin: 15px; height: 80px">
+#
+# # Project 3
+#
+# ### Regression and Classification with the Ames Housing Data
+#
+# ---
+#
+# You have just joined a new "full stack" real estate company in Ames, Iowa. The strategy of the firm is two-fold:
+# - Own the entire process from the purchase of the land all the way to sale of the house, and anything in between.
+# - Use statistical analysis to optimize investment and maximize return.
+#
+# The company is still small, and though investment is substantial the short-term goals of the company are more oriented towards purchasing existing houses and flipping them as opposed to constructing entirely new houses. That being said, the company has access to a large construction workforce operating at rock-bottom prices.
+#
+# This project uses the [Ames housing data recently made available on kaggle](https://www.kaggle.com/c/house-prices-advanced-regression-techniques).
+
+# +
+import numpy as np
+import scipy.stats as stats
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+
+sns.set_style('whitegrid')
+
+# %config InlineBackend.figure_format = 'retina'
+# %matplotlib inline
+# -
+
+# <img src="http://imgur.com/l5NasQj.png" style="float: left; margin: 25px 15px 0px 0px; height: 25px">
+#
+# ## 1. Estimating the value of homes from fixed characteristics.
+#
+# ---
+#
+# Your superiors have outlined this year's strategy for the company:
+# 1. Develop an algorithm to reliably estimate the value of residential houses based on *fixed* characteristics.
+# 2. Identify characteristics of houses that the company can cost-effectively change/renovate with their construction team.
+# 3. Evaluate the mean dollar value of different renovations.
+#
+# Then we can use that to buy houses that are likely to sell for more than the cost of the purchase plus renovations.
+#
+# Your first job is to tackle #1. You have a dataset of housing sale data with a huge amount of features identifying different aspects of the house. The full description of the data features can be found in a separate file:
+#
+#     housing.csv
+#     data_description.txt
+#     
+# You need to build a reliable estimator for the price of the house given characteristics of the house that cannot be renovated. Some examples include:
+# - The neighborhood
+# - Square feet
+# - Bedrooms, bathrooms
+# - Basement and garage space
+#
+# and many more. 
+#
+# Some examples of things that **ARE renovate-able:**
+# - Roof and exterior features
+# - "Quality" metrics, such as kitchen quality
+# - "Condition" metrics, such as condition of garage
+# - Heating and electrical components
+#
+# and generally anything you deem can be modified without having to undergo major construction on the house.
+#
+# ---
+#
+# **Your goals:**
+# 1. Perform any cleaning, feature engineering, and EDA you deem necessary.
+# - Be sure to remove any houses that are not residential from the dataset.
+# - Identify **fixed** features that can predict price.
+# - Train a model on pre-2010 data and evaluate its performance on the 2010 houses.
+# - Characterize your model. How well does it perform? What are the best estimates of price?
+#
+# > **Note:** The EDA and feature engineering component to this project is not trivial! Be sure to always think critically and creatively. Justify your actions! Use the data description file!
+
+# Load the data
+house = pd.read_csv('./housing.csv')
+
+# +
+# A:
+# -
+
+# <img src="http://imgur.com/l5NasQj.png" style="float: left; margin: 25px 15px 0px 0px; height: 25px">
+#
+# ## 2. Determine any value of *changeable* property characteristics unexplained by the *fixed* ones.
+#
+# ---
+#
+# Now that you have a model that estimates the price of a house based on its static characteristics, we can move forward with part 2 and 3 of the plan: what are the costs/benefits of quality, condition, and renovations?
+#
+# There are two specific requirements for these estimates:
+# 1. The estimates of effects must be in terms of dollars added or subtracted from the house value. 
+# 2. The effects must be on the variance in price remaining from the first model.
+#
+# The residuals from the first model (training and testing) represent the variance in price unexplained by the fixed characteristics. Of that variance in price remaining, how much of it can be explained by the easy-to-change aspects of the property?
+#
+# ---
+#
+# **Your goals:**
+# 1. Evaluate the effect in dollars of the renovate-able features. 
+# - How would your company use this second model and its coefficients to determine whether they should buy a property or not? Explain how the company can use the two models you have built to determine if they can make money. 
+# - Investigate how much of the variance in price remaining is explained by these features.
+# - Do you trust your model? Should it be used to evaluate which properties to buy and fix up?
+
+# +
+# A:
+# -
+
+# <img src="http://imgur.com/GCAf1UX.png" style="float: left; margin: 25px 15px 0px 0px; height: 25px">
+#
+# ## 3. What property characteristics predict an "abnormal" sale?
+#
+# ---
+#
+# The `SaleCondition` feature indicates the circumstances of the house sale. From the data file, we can see that the possibilities are:
+#
+#        Normal	Normal Sale
+#        Abnorml	Abnormal Sale -  trade, foreclosure, short sale
+#        AdjLand	Adjoining Land Purchase
+#        Alloca	Allocation - two linked properties with separate deeds, typically condo with a garage unit	
+#        Family	Sale between family members
+#        Partial	Home was not completed when last assessed (associated with New Homes)
+#        
+# One of the executives at your company has an "in" with higher-ups at the major regional bank. His friends at the bank have made him a proposal: if he can reliably indicate what features, if any, predict "abnormal" sales (foreclosures, short sales, etc.), then in return the bank will give him first dibs on the pre-auction purchase of those properties (at a dirt-cheap price).
+#
+# He has tasked you with determining (and adequately validating) which features of a property predict this type of sale. 
+#
+# ---
+#
+# **Your task:**
+# 1. Determine which features predict the `Abnorml` category in the `SaleCondition` feature.
+# - Justify your results.
+#
+# This is a challenging task that tests your ability to perform classification analysis in the face of severe class imbalance. You may find that simply running a classifier on the full dataset to predict the category ends up useless: when there is bad class imbalance classifiers often tend to simply guess the majority class.
+#
+# It is up to you to determine how you will tackle this problem. I recommend doing some research to find out how others have dealt with the problem in the past. Make sure to justify your solution. Don't worry about it being "the best" solution, but be rigorous.
+#
+# Be sure to indicate which features are predictive (if any) and whether they are positive or negative predictors of abnormal sales.
+
+# +
+# A:
